5d822a1beabd6a48fc2d20675155a663e4c459aa
戴文渝 <wenyu.dai@qq.com>
2020-07-24 17:16:20 +0800

    增加国际化

--- src/pages/SystemAdmin/Menu/index.tsx
+++ src/pages/SystemAdmin/Menu/index.tsx
@@ -1,19 +1,33 @@
-import {PageContainer} from "@ant-design/pro-layout";
-import React, {useEffect, useState} from "react";
-import {Card, Row, Col, Menu, Button, Modal, Form, Input, TreeSelect, Divider, Tree, Select} from "antd";
-import {connect} from "umi";
-import {SystemModelState} from "@/models/connect";
-import {Dispatch} from "@@/plugin-dva/connect";
-import {createFromIconfontCN, ExclamationCircleOutlined} from '@ant-design/icons';
+import { PageContainer } from "@ant-design/pro-layout";
+import React, { useEffect, useState } from "react";
+import {
+  Card, Row, Col, Menu, Button, Modal, Form,
+  Input, TreeSelect, Divider, Tree, Select,
+  Radio,
+  Space
+} from "antd";
+import { connect } from "umi";
+import { SystemModelState } from "@/models/connect";
+import { Dispatch } from "@@/plugin-dva/connect";
+import { createFromIconfontCN, ExclamationCircleOutlined } from '@ant-design/icons';
 
-const {SubMenu} = Menu;
-const {confirm} = Modal;
+const { SubMenu } = Menu;
+const { confirm } = Modal;
 const Icon = createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/font_1958855_2w455u5pd5j.js',
 });
-const IconType = ["icon-earth", "icon-message", "icon-dashboard", "icon-piechart", "icon-setting", "icon-eye", "icon-location", "icon-edit-square", "icon-save", "icon-appstore", "icon-layout", "icon-play-square", "icon-control", "icon-codelibrary", "icon-detail", "icon-project", "icon-wallet", "icon-calculator", "icon-interation", "icon-user", "icon-team", "icon-areachart", "icon-linechart", "icon-barchart", "icon-pointmap", "icon-alert", "icon-bulb", "icon-experiment", "icon-bell", "icon-home", "icon-shop", "icon-fund", "icon-desktop", "icon-Partition", "icon-index", "icon-Console-SQL"];
+const IconType = [
+  "icon-earth", "icon-message", "icon-dashboard", "icon-piechart", "icon-setting",
+  "icon-eye", "icon-location", "icon-edit-square", "icon-save", "icon-appstore",
+  "icon-layout", "icon-play-square", "icon-control", "icon-codelibrary", "icon-detail",
+  "icon-project", "icon-wallet", "icon-calculator", "icon-interation", "icon-user",
+  "icon-team", "icon-areachart", "icon-linechart", "icon-barchart", "icon-pointmap",
+  "icon-alert", "icon-bulb", "icon-experiment", "icon-bell", "icon-home",
+  "icon-shop", "icon-fund", "icon-desktop", "icon-Partition", "icon-index",
+  "icon-Console-SQL"
+];
+// Array.from(document.querySelectorAll('.icon-code.icon-code-show')).map(v=>v.innerText)
 
-//Array.from(document.querySelectorAll('.icon-code.icon-code-show')).map(v=>v.innerText)
 
 export interface MenuAdminProps {
   dispatch: Dispatch;
@@ -26,7 +40,7 @@ function makeMenu(data) {
         <SubMenu
           title={v.menuName}
           key={v.menuId}
-          icon={<Icon type={v.menuIcon}/>}
+          icon={<Icon type={v.menuIcon} />}
         >
           {makeMenu(v.children)}
         </SubMenu>
@@ -35,7 +49,7 @@ function makeMenu(data) {
     return (
       <Menu.Item
         key={v.menuId}
-        icon={<Icon type={v.menuIcon}/>}
+        icon={<Icon type={v.menuIcon} />}
       >
         {v.menuName}
       </Menu.Item>
@@ -45,8 +59,11 @@ function makeMenu(data) {
 }
 
 function MenuAdmin(props: MenuAdminProps) {
-  const {dispatch} = props;
+  const { dispatch } = props;
   const [form] = Form.useForm();
+
+  // 国际化
+  const [lang, setLang] = useState(localStorage.getItem('YY-local') || 'zh-CN');
 
   // 菜单数据库原始数据
   const [menuData, setMenuData] = useState([]);
@@ -66,87 +83,81 @@ function MenuAdmin(props: MenuAdminProps) {
   // 当前选中菜单
   const [activeData, setActiveData] = useState(undefined);
 
+  function getLocalStr(data) {
+    try {
+      return JSON.parse(data)[lang];
+    } catch (e) {
+      console.log(data, lang)
+      return '-';
+    }
+  }
+
   const getMenusData = async () => {
     const data = await dispatch({
       type: 'system/getMenus'
     }) || [];
+
+    // 存储原始数据
     setMenuData([...data]);
 
-    const treeData = [];
-    const recursiveData = (arr, item) => {
-      for (let i = 0; i < arr.length; i++) {
-        const v = arr[i];
-        if (v.menuId === item.parentId) {
-          if (!Array.isArray(v.children)) {
-            v.children = [];
+    // 数据组装
+    const treeArr = [];
+    data.forEach(v => {
+      const sortArr = v.sort.split('-');
+      const pushData = (indexArr, dataArr) => {
+        const index = indexArr.shift();
+        if (indexArr.length === 0) {
+          const rawData = {
+            ...v,
+            title: getLocalStr(v.menuName),
+            value: v.menuId,
+            key: v.menuId,
+            menuName: getLocalStr(v.menuName),
+            icon: (<Icon type={v.menuIcon} />),
+            children: []
           }
-          v.children.push(item);
-          return true;
-        }
-        if (Array.isArray(v.children)) {
-          const result = recursiveData(v.children, item);
-          if (result === true) {
-            return true;
+          if (dataArr[index] && dataArr[index].children) {
+            dataArr[index] = {
+              ...rawData,
+              ...dataArr[index],
+            }
+          } else {
+            dataArr[index] = {
+              ...rawData
+            };
           }
+          return;
         }
+        if (dataArr[index] === undefined) {
+          dataArr[index] = {
+            children: []
+          };
+        }
+
+        pushData(indexArr, dataArr[index].children);
       }
-    }
-
-    let count = 0;
-    while (data.length && count < 20) {
-      for (let i = 0; i < data.length; i++) {
-        data[i] = {
-          ...data[i],
-          title: data[i].menuName,
-          value: data[i].menuId,
-          key: data[i].menuId,
-          icon: (<Icon type={data[i].menuIcon}/>)
-        };
-        if (data[i].parentId) {
-          const result = recursiveData(treeData, data[i]);
-          if (result) {
-            data.splice(i, 1);
-            break;
-          }
-        } else {
-          treeData.push(data[i]);
-
-          data.splice(i, 1);
-          break;
-        }
-      }
-
-      count++;
-    }
-    // console.log(treeData);
-    // treeData排序
-    const sortData = (arr) => {
-      arr.sort((a, b) => {
-        if (a.sort < b.sort) {
-          return -1;
-        }
-        if (a.sort > b.sort) {
-          return 1;
-        }
-        return 0;
-      });
-      arr.forEach(v => {
-        if (Array.isArray(v.children) && v.children.length) {
-          sortData(v.children);
-        }
-      });
-    }
-    sortData(treeData)
-    setMenu(treeData);
+      pushData(sortArr, treeArr);
+    });
+    setMenu(treeArr);
   }
 
   const addMenu = () => {
     const getSortValue = (parentId) => {
-      if (parentId === undefined) {
-        return menu.length.toString()
+      // FIXME 存在原始数据时返回数据不正确
+
+      if (typeof parentId !== 'number') {
+        // 一级菜单
+        if (activeData) {
+          // 修改
+          return activeData.sort;
+        }
+        // 新增
+        return menu.length.toString();
+
       }
+
       const parentSort = menuData.find(v => v.menuId === parentId).sort;
-      const itemData = menuData.filter(v => v.sort.includes(parentSort + '-', 0));
+      const itemData = menuData.filter(v => v.sort.includes(`${parentSort}-`, 0));
       // console.log(itemData);
       return `${parentSort}-${itemData.length}`;
     }
@@ -154,14 +165,17 @@ function MenuAdmin(props: MenuAdminProps) {
     form
       .validateFields()
       .then(async values => {
+        console.log(values);
+        // return;
         if (activeData) {
           // 修改
           await dispatch({
             type: 'system/updateMenu',
             payload: {
+              ...values,
               id: activeData.menuId,
               sort: getSortValue(values.parentId),
-              ...values
+              menuName: JSON.stringify(values.menuName)
             }
           });
         } else {
@@ -169,8 +183,9 @@ function MenuAdmin(props: MenuAdminProps) {
           await dispatch({
             type: 'system/addMenu',
             payload: {
+              ...values,
               sort: getSortValue(values.parentId),
-              ...values
+              menuName: JSON.stringify(values.menuName)
             }
           });
         }
@@ -243,24 +258,33 @@ function MenuAdmin(props: MenuAdminProps) {
   };
 
   const onSelectTree = (selectedKeys, e) => {
+    form.resetFields();
     if (selectedKeys.length === 1) {
       const currentData = menuData.find(v => selectedKeys[0] === v.menuId);
+      const menuName = (() => {
+        try {
+          return JSON.parse(currentData.menuName);
+        } catch (e) {
+          return {};
+        }
+      })();
+      console.log(menuName)
       setActiveData(currentData);
+
       form.setFieldsValue({
         parentId: currentData.parentId,
-        menuName: currentData.menuName,
+        menuName,
         menuIcon: currentData.menuIcon,
         menuPath: currentData.menuPath
       });
     } else {
-      form.resetFields();
       setActiveData(undefined);
     }
   }
 
   const onDelete = () => {
     const currentId = activeData.menuId;
-    let deleteData = [];
+    const deleteData = [];
 
     const findDeleteData = (arr, flag) => {
       for (let i = 0; i < arr.length; i++) {
@@ -275,7 +299,7 @@ function MenuAdmin(props: MenuAdminProps) {
     findDeleteData(menu);
     confirm({
       title: '确认删除该菜单？',
-      icon: <ExclamationCircleOutlined/>,
+      icon: <ExclamationCircleOutlined />,
       content: deleteData.length > 1 ? '该菜单下存在子菜单，删除该菜单，其子菜单会同时删除' : null,
       async onOk() {
         await dispatch({
@@ -299,7 +323,7 @@ function MenuAdmin(props: MenuAdminProps) {
     const diffArr = [];
     let level = 0;
     const checkSort = (arr, inds, parentId) => {
-      let indexArr = [].concat(inds)
+      const indexArr = [].concat(inds)
       for (let i = 0; i < arr.length; i++) {
         indexArr[level] = i;
         // 数据对比
@@ -329,7 +353,7 @@ function MenuAdmin(props: MenuAdminProps) {
     await dispatch({
       type: 'system/updateMenuSort',
       payload: {
-        diffArr: diffArr
+        diffArr
       }
     })
 
@@ -342,6 +366,10 @@ function MenuAdmin(props: MenuAdminProps) {
   useEffect(() => {
     getMenusData();
   }, []);
+
+  useEffect(() => {
+    getMenusData();
+  }, [lang]);
 
   return (
     <PageContainer>
@@ -359,7 +387,7 @@ function MenuAdmin(props: MenuAdminProps) {
             >
               排序
             </Button>
-            <Divider type="vertical"/>
+            <Divider type="vertical" />
             <Button
               ghost
               type="primary"
@@ -372,21 +400,31 @@ function MenuAdmin(props: MenuAdminProps) {
       >
         <Row gutter={16}>
           <Col span={6}>
-            {
-              menu.length ? (
-                <Tree
-                  showIcon
-                  //draggable
-                  blockNode
-                  defaultExpandAll
-                  treeData={menu}
-                  //onDrop={onDrop}
-                  onSelect={onSelectTree}
-                />
-              ) : null
-            }
+            <Space direction="vertical">
+              <Radio.Group
+                buttonStyle="solid"
+                value={lang}
+                onChange={e => setLang(e.target.value)}
+              >
+                <Radio.Button value="zh-CN">中文简体</Radio.Button>
+                <Radio.Button value="zh-TW">中文繁體</Radio.Button>
+                <Radio.Button value="en-US">English</Radio.Button>
+              </Radio.Group>
+
+              {
+                menu.length ? (
+                  <Tree
+                    showIcon
+                    blockNode
+                    defaultExpandAll
+                    treeData={menu}
+                    onSelect={onSelectTree}
+                  />
+                ) : null
+              }
+            </Space>
           </Col>
-          <Col span={10}>
+          <Col span={8}>
             <Form
               form={form}
               layout="vertical"
@@ -403,24 +441,40 @@ function MenuAdmin(props: MenuAdminProps) {
                 />
               </Form.Item>
 
-              <Form.Item
-                label="菜单名称"
-                name="menuName"
-                rules={[{required: true, message: 'Please input menuName!'}]}
-              >
-                <Input/>
+              <Form.Item label="菜单名称" required>
+                <Form.Item
+                  name={['menuName', 'zh-CN']}
+                >
+                  <Input
+                    placeholder="请输入简体菜单名称"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name={['menuName', 'zh-TW']}
+                >
+                  <Input
+                    placeholder="請輸入繁體菜單名稱"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name={['menuName', 'en-US']}
+                >
+                  <Input
+                    placeholder="Please enter the English name of the menu"
+                  />
+                </Form.Item>
               </Form.Item>
 
               <Form.Item
                 label="菜单图标"
                 name="menuIcon"
-                rules={[{required: true, message: 'Please input menuIcon!'}]}
+                rules={[{ required: true, message: 'Please input menuIcon!' }]}
               >
                 <Select allowClear>
                   {
                     IconType.map(v => (
                       <Select.Option key={v} value={v}>
-                        <Icon type={v}/>
+                        <Icon type={v} />
                       </Select.Option>
                     ))
                   }
@@ -430,9 +484,9 @@ function MenuAdmin(props: MenuAdminProps) {
               <Form.Item
                 label="菜单路径"
                 name="menuPath"
-                rules={[{required: true, message: 'Please input menuPath!'}]}
+                rules={[{ required: true, message: 'Please input menuPath!' }]}
               >
-                <Input/>
+                <Input />
               </Form.Item>
 
               <Form.Item>
@@ -444,7 +498,7 @@ function MenuAdmin(props: MenuAdminProps) {
                     activeData ? '更新' : '添加'
                   }
                 </Button>
-                <Divider type="vertical"/>
+                <Divider type="vertical" />
                 {
                   activeData ? (
                     <Button
@@ -455,12 +509,12 @@ function MenuAdmin(props: MenuAdminProps) {
                       删除
                     </Button>
                   ) : (
-                    <Button
-                      onClick={() => form.resetFields()}
-                    >
-                      重置
-                    </Button>
-                  )
+                      <Button
+                        onClick={() => form.resetFields()}
+                      >
+                        重置
+                      </Button>
+                    )
                 }
               </Form.Item>
             </Form>
@@ -480,7 +534,7 @@ function MenuAdmin(props: MenuAdminProps) {
             <Menu
               mode="inline"
               theme="dark"
-              style={{minHeight: 500}}
+              style={{ minHeight: 500 }}
             >
               {makeMenu(menu)}
             </Menu>
@@ -516,4 +570,4 @@ function MenuAdmin(props: MenuAdminProps) {
   );
 }
 
-export default connect(({system}: SystemModelState) => ({system}))(MenuAdmin);
+export default connect(({ system }: SystemModelState) => ({ system }))(MenuAdmin);
